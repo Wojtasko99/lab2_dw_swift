cf53a0132dde8665c99ed6225148278377511427
Damian Wojtal <damianero90@gmail.com>
2024-04-27 14:45:38 +0200

    Commit test

---
+++ lab2_dw_swift/Card.swift
@@ -0,0 +1,18 @@
+//
+//  Card.swift
+//  lab2_dw_swift
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+struct Card: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 12)
+    }
+}
+
+#Preview {
+    Card()
+}
