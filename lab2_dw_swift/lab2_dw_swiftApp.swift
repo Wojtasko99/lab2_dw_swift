4d21d7f83ae43adc09c88650a57fe84a33716002
Damian Wojtal <damianero90@gmail.com>
2024-04-27 14:20:28 +0200

    Initial Commit

---
+++ lab2_dw_swift/lab2_dw_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  lab2_dw_swiftApp.swift
+//  lab2_dw_swift
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct lab2_dw_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
