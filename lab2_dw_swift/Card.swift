c18599a882c2b27dbaec1184875c6e4c7c5f7c24
Damian Wojtal <damianero90@gmail.com>
2024-04-27 16:31:26 +0200

    CardViews

--- lab2_dw_swift/Card.swift
+++ lab2_dw_swift/Card.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct Card: View {
-    var body: some View {
-        RoundedRectangle(cornerRadius: 12)
+    @State var show: Bool = true;
+    @State var icon: String;
+    init(icon: String) {
+        self.icon = icon
     }
-}
-
-#Preview {
-    Card()
+    
+    var body: some View {
+        Group{
+            ZStack{
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(.white)
+                    .frame(height: 120)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(.blue,lineWidth: 2)
+                    )
+                    .onTapGesture {
+                        show = !show
+                    }
+                Text(icon).font(.largeTitle)
+                    RoundedRectangle(cornerRadius: 12)
+                        .fill(.blue)
+                        .frame(height: 120)
+                        .opacity(show ? 1 : 0)
+                        .onTapGesture {
+                            show = !show
+                        
+                }
+            }
+        }.opacity(1)
+        
+            
+            
+    }
 }
--- lab2_dw_swift/ContentView.swift
+++ lab2_dw_swift/ContentView.swift
@@ -8,17 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var icons: [String] = ["🥰","😀","👺","👻","😈","🎃","😇","🤣","😝","🦉","🧂","🥃"]
+    @State var numberOfCardsDisplayed: Int = 6
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]){
+                    ForEach(icons.prefix(numberOfCardsDisplayed), id: \.self) { icon in
+                        Card(icon: icon)
+                    }
+                }
+                    .foregroundColor(.blue)
+                    .padding()
+            }
+            HStack {
+                Button(action: {
+                    adjustCardNumber(by: -2)
+                }) {
+                    Image(systemName: "minus.circle.fill")
+                        .font(.title)
+                }
+                .disabled(numberOfCardsDisplayed <= 2)
+                
+                
+                Button(action: {
+                    adjustCardNumber(by: 2)
+                }) {
+                    Image(systemName: "plus.circle.fill")
+                        .font(.title)
+                }
+                .disabled(numberOfCardsDisplayed >= icons.count)
+            }
+            .padding()
         }
-        .padding()
+    }
+    
+    func adjustCardNumber(by offset: Int) {
+        let newNumber = numberOfCardsDisplayed + offset
+        if newNumber >= 2 && newNumber <= icons.count {
+            numberOfCardsDisplayed = newNumber
+        }
     }
 }
 
-#Preview {
-    ContentView()
-}
